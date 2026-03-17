dc34a4a4a6dcf34012d2bde9888edc77dbbc364e
aymenesamri-bit <aymenesamri@gmail.com>
2026-03-17 14:52:25 +0100

    Add password testing form in test_password.php

---
+++ test_password.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulaire mot de passe</title>
+</head>
+<body>
+
+<h2>Tester un mot de passe</h2>
+
+<form action="test_password.php" method="post">
+    
+    <label>Mot de passe :</label><br>
+    <input type="text" name="password"><br><br>
+
+    <button type="submit">Valider</button>
+
+</form>
+
+</body>
